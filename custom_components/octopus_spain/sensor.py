2ed1fc5d5495ae90b724242f1ebf812df6d4faaa
Mguel Ángel <miguel.angel.lopez.vicente@gmail.com>
2023-06-08 11:32:14 +0200

    Update invoces

--- custom_components/octopus_spain/sensor.py
+++ custom_components/octopus_spain/sensor.py
@@ -106,6 +106,7 @@ class OctopusInvoice(CoordinatorEntity, SensorEntity):
         )
 
     async def async_added_to_hass(self) -> None:
+        await super().async_added_to_hass()
         self._handle_coordinator_update()
 
     @callback
